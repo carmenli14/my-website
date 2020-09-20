2a0ccd1ab4ca5befa1578e30e9ee82d0c9cc9c80
Carmen Li <carmenli@Carmens-MacBook-Pro-3.local>
2020-09-20 15:00:09 -0700

    Updates

---
+++ precache-manifest.456fd3e43db91f1dcaa47e100a7c7ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4597e0b7f1f54f3b51a991bfb5fc678",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "c25653c4669cbc8177ef",
+    "url": "/my-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "204a3a0cae7b62f9b255",
+    "url": "/my-website/static/js/2.5f7d4ff1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.5f7d4ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25653c4669cbc8177ef",
+    "url": "/my-website/static/js/main.aa5098bd.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  }
+]);
